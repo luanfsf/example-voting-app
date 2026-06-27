ba84892613b83f8d07c90f3372495b66a1d480c1
Luan Ferreira <luanfsf@gmail.com>
2026-06-26 21:18:50 -0300

    rollback voting changes and removed sonar steps

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_b = os.getenv('OPTION_B', "IntelliJ")
-option_a = os.getenv('OPTION_A', "VSCode")
+option_a = os.getenv('OPTION_A', "Emacs")
+option_b = os.getenv('OPTION_B', "Vi")
 hostname = socket.gethostname()
 version = 'v1'
 
